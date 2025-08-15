59be6d222db33c90fbc4d48deb1bb3112571d45c
Diogo <diogoteixeiralucas@pm.me>
2025-08-15 08:51:56 +0100

    Add constants for colors, spacings, and typography

---
+++ PillUp/Source/Constants/Spacings.swift
@@ -0,0 +1,18 @@
+//
+//  Spacings.swift
+//  PillUp
+//
+//  Created by Diogo on 15/08/2025.
+//
+
+
+import Foundation
+import UIKit
+
+public struct Spacing {
+    public static var huge: CGFloat = 48
+    public static var medium: CGFloat = 24
+    public static var small: CGFloat = 12
+    public static var little: CGFloat = 8
+    public static var tiny: CGFloat = 4
+}
