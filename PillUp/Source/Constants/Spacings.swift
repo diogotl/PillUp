65726db3e74f17c316cf5cdf9485b728361a0d56
Diogo <diogoteixeiralucas@pm.me>
2025-08-17 22:17:08 +0100

    Add UI components, Core Data model, and constants

--- PillUp/Source/Constants/Spacings.swift
+++ PillUp/Source/Constants/Spacings.swift
@@ -15,4 +15,8 @@ public struct Spacing {
     public static var small: CGFloat = 12
     public static var little: CGFloat = 8
     public static var tiny: CGFloat = 4
+    public static var buttonSize: CGFloat = 56
+    public static var inputSize: CGFloat = 56
+    public static var profileImageSize: CGFloat = 68
+    public static var backgroundProfileSize: CGFloat = 240
 }
